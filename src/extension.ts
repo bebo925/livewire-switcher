c76ca702978b16b02dcbd403dff4bd9ed52c21c8
Ryan McQuerry <bebo925@gmail.com>
2022-02-10 11:17:45 -0500

    fix switching bug with complicated file names

--- src/extension.ts
+++ src/extension.ts
@@ -18,16 +18,19 @@ export function activate(context: vscode.ExtensionContext) {
       if (vscode.workspace.asRelativePath(vscode.window.activeTextEditor?.document.fileName).startsWith('resources/views/livewire/')) {
         const relativePath = vscode.window.activeTextEditor?.document.fileName.split('resources/views/livewire/')[1];
         let file = relativePath
+          .replace('.blade.php', '')
           .split('/')
-          .map((d, index, array) => {
-            if (index === array.length - 1) {
-              let c = d.split('.blade').join('');
-              return c.charAt(0).toUpperCase() + c.slice(1);
-            } else {
-              return d.charAt(0).toUpperCase() + d.slice(1);
-            }
+          .map((folder, index, array) => {
+            return folder
+              .split('-')
+              .map((word, index, array) => {
+                return word.charAt(0).toUpperCase() + word.slice(1);
+              })
+              .join('');
           })
-          .join('/');
+          .join('/')
+          .concat('.php');
+
         const document = await vscode.workspace.openTextDocument(vscode.workspace.workspaceFolders[0].uri.path + '/app/Http/Livewire/' + file);
         await vscode.window.showTextDocument(document);
       } else if (vscode.workspace.asRelativePath(vscode.window.activeTextEditor?.document.fileName).startsWith('app/Http/Livewire/')) {
@@ -35,14 +38,21 @@ export function activate(context: vscode.ExtensionContext) {
         let file = relativePath
           .split('/')
           .map((d, index, array) => {
-            if (index === array.length - 1) {
-              let c = d.split('.php')[0] + '.blade.php';
-              return c.toLocaleLowerCase();
-            } else {
-              return d.toLocaleLowerCase();
-            }
+            return d
+              .replace(/\.[^/.]+$/, '')
+              .split('')
+              .reduce((word: string[], letter: string) => {
+                if (/[A-Z]/.test(letter) && word.length > 0) {
+                  word.push('-');
+                }
+                word.push(letter.toLowerCase());
+                return word;
+              }, [])
+              .join('');
           })
-          .join('/');
+          .join('/')
+          .concat('.blade.php');
+
         const document = await vscode.workspace.openTextDocument(vscode.workspace.workspaceFolders[0].uri.path + '/resources/views/livewire/' + file);
         await vscode.window.showTextDocument(document);
       }
